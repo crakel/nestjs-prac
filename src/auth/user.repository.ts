1eb2bbfe752e4dd89d0c68e03e629f2dc4d0f39c
김기윤 <crakel528@ajou.ac.kr>
2022-03-13 21:43:35 +0900

    feat : signIn

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,8 @@
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
+
 import {
   ConflictException,
   InternalServerErrorException,
@@ -11,7 +13,10 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentailsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentailsDto;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
